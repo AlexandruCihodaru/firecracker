93a5bb534f694124f4df60127c846ae1518c205c
AlexandruCihodaru <cihodar@amazon.com>
2021-06-15 16:03:44 +0300

    [CI]: Add tests for CPUID exposed features
    Signed-off-by: AlexandruCihodaru <cihodar@amazon.com>

--- tests/integration_tests/functional/test_cpu_features.py
+++ tests/integration_tests/functional/test_cpu_features.py
@@ -170,6 +170,49 @@ def test_cpu_template(test_microvm_with_ssh, network_config, cpu_template):
                                     "XSAVEC": "false", "XGETBV": "false",
                                     "XSAVES": "false"}
 
+    enabled_features_cpuid = {"x87": "true", "RDTSCP": "true",
+                              "virtual-8086": "true", "debugging": "true",
+                              "page size": "true", "time": "true",
+                              "RDMSR": "true", "machine": "true",
+                              "physical address extensions": "true",
+                              "CMPXCHG8B": "true", "PTE": "true",
+                              "SYSENTER and SYSEXIT": "true",
+                              "machine check architecture": "true",
+                              "conditional move/compare instruction": "true",
+                              "page attribute table": "true", "MMX": "true",
+                              "page size extension": "true", "SSE": "true",
+                              "CLFLUSH instruction": "true", "memory": "true",
+                              "PNI/SSE3:": "true", "PCLMULDQ": "true",
+                              "CMPXCHG16B": "true", "APIC on chip": "true",
+                              "SSSE3": "true", "SSE4.1": "true",
+                              "process context identifiers": "true",
+                              "extended xAPIC support": "true",
+                              "time stamp counter deadline": "true",
+                              "AES": "true", "TscInvariant": "true",
+                              "XSAVE/XSTOR states": "true",
+                              "OS-enabled XSAVE/XSTOR": "true",
+                              "AVX": "true", "F16C": "true", "RDRAND": "true",
+                              "hypervisor guest status": "true",
+                              "ARAT": "true", "FSGSBASE": "true",
+                              "IA32_TSC_ADJUST MSR supported": "true",
+                              "enhanced REP MOVSB/STOSB": "true",
+                              "SMAP": "true", "SMEP": "true", "POPCNT": "true",
+                              "XCR0 supported: x87 state": "true",
+                              "XCR0 supported: SSE state": "true",
+                              "XCR0 supported: AVX state": "true",
+                              "XSAVEOPT instruction": "true",
+                              "kvmclock available at MSR 0x11": "true",
+                              "delays unnecessary for PIO ops": "true",
+                              "kvmclock available a MSR 0x4b564d00": "true",
+                              "async pf enable available by MSR": "true",
+                              "steal clock supported": "true",
+                              "guest EOI optimization enabled": "true",
+                              "stable: no guest per-cpu warps": "true",
+                              "SYSCALL and SYSRET": "true", "SSE4.2": "true",
+                              "execution disable": "true",
+                              "64-bit extensions technology available": "true",
+                              "LAHF/SAHF supported in 64-bit mode": "true",
+                              "FXSAVE/FXRSTOR": "true"}
     # These are all discoverable by cpuid -1.
     c3_masked_features = {"FMA": "false", "MOVBE": "false", "BMI": "false",
                           "AVX2": "false", "BMI2": "false", "INVPCID": "false"}
@@ -206,7 +249,6 @@ def test_cpu_template(test_microvm_with_ssh, network_config, cpu_template):
     assert stderr.read() == ''
 
     cpu_flags_output = stdout.readline().rstrip().split(' ')
-
     for feature in common_masked_features_lscpu:
         assert feature not in cpu_flags_output, feature
 
@@ -226,3 +268,6 @@ def test_cpu_template(test_microvm_with_ssh, network_config, cpu_template):
 
     utils.check_guest_cpuid_output(test_microvm, "cpuid -1", None, '=',
                                    expected_cpu_features)
+
+    utils.check_guest_cpuid_output(test_microvm, "cpuid -1", None, '=',
+                                   enabled_features_cpuid)
